5647be6cd1163ccee760ac521f6d2df30a005569
DeValladolid <devalladolidoficial@gmail.com>
2018-09-27 10:59:03 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = 'September 27 2018 08:00:00 GMT+0000';
+var deadline = 'December 27 2018 08:00:00 GMT+0000';
 initializeClock('clockdiv', deadline);
